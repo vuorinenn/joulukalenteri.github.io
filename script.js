8796fb2f9338203b6da3cf1344a6c96fea7e7ddb
mk <miksuvuor@gmail.com>
2025-11-20 14:28:12 +0200

    Create advent calendar messages in script.js
    Initialize an advent calendar with messages for each day.

---
+++ script.js
@@ -0,0 +1,33 @@
+const viestit = [
+    "1. Hyvää joulukuun ensimmäistä päivää!",
+    "2. Muista syödä pipareita tänään!",
+    "3. Joululaulut tuovat iloa päivääsi.",
+    "4. On aika koristella joulukuusi!",
+    "5. Joululahjojen ostaminen on hauskaa!",
+    "6. Nauti kuumaa kaakaota kylmänä päivänä.",
+    "7. Tee joulukortteja ystäville ja perheelle.",
+    "8. Katso jouluelokuva tänään.",
+    "9. Leivo joulutorttuja yhdessä perheen kanssa.",
+    "10. Joulun taika on kaikkialla ympärillämme.",
+    "11. Muista antaa aikaa läheisillesi.",
+    "12. Joulun odotus tekee sydämestä lämpimän.",
+    "13. Käy joulumarkkinoilla nauttimassa tunnelmasta.",
+    "14. Joulun värit tuovat iloa silmille.",
+    "15. Tee hyvää ja auta tarvitsevia.",
+    "16. Joulun tuoksut täyttävät kodin.",
+    "17. Valmista jouluateria yhdessä perheen kanssa.",
+    "18. Joulun aika on antamisen aikaa.",
+    "19. Nauti talvisista maisemista ulkona.",
+    "20. Joulun valot loistavat pimeydessä.",
+    "21. Muista rentoutua ja nauttia joulun tunnelmasta.",
+    "22. Joulupukki on matkalla!",
+    "23. Valmistaudu jouluaattoon innolla.",
+    "24. Hyvää joulua ja onnellista uutta vuotta!"
+];
+
+const kalenteri = document.getElementById("kalenteri");
+
+for (let i = 1; i <= 24; i++) {
+    const ovi = document.createElement("div");
+    ovi.className = 'ovi';
+}
